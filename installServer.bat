f3282208f646f0cf320ae408027b60bb681a28cc
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-05-22 16:00:50 +0200

    install script

---
+++ installServer.bat
@@ -0,0 +1,10 @@
+mkdir collectitemchallenge
+cd collectitemchallenge
+curl https://api.papermc.io/v2/projects/paper/versions/1.19.4/builds/538/downloads/paper-1.19.4-538.jar >> paper.jar
+mkdir plugins
+cd plugins
+curl https://maven.blueamethyst.me/releases/tech/marlonr/collectitemchallenge/1.0/collectitemchallenge-1.0.jar >> collectitemchallenge-1.0.jar
+cd ..
+echo java -Xmx3G -jar paper.jar nogui > start.bat
+echo Now you can start the server with the start.bat!
+pause
